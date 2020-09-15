390af1ee70aa7018283b6d41e74268371d83b26d
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-09-15 16:11:05 -0400

    backend may be done

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,65 @@
+class CommentsController < ApplicationController
+  before_action :authorize_request, only: [:create, :update, :destroy]
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  # GET /comments
+  def index
+    @comments = Comment.all
+
+    render json: @comments
+  end
+
+  # GET /comments/1
+  def show
+    render json: @comment
+  end
+
+  # POST /comments
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /comments/1
+  def destroy
+    @comment.destroy
+  end
+
+  def add_comment_to_post
+    @post = Post.find(params[:id])
+
+    @comment = Comment.new(params.require(:comment).permit(:content, :post_id, :user_id))
+    if @comment.save
+      @post.comments << @comment
+      render json: @post, include: :comments
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def comment_params
+      params.require(:comment).permit(:content)
+      # params.require(:comment).permit(:content, :post_id, :user_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   resources :users
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
+  post '/posts/:id/comments', to: 'comments#add_comment_to_post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
